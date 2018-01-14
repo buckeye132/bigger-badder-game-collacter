a551d04afb60cb23f9044ced37ffc2c89b1a1d3e
buckeye132 <adamkane@gmail.com>
2018-01-13 19:37:37 -0800

    added add game button, add game modal form

--- web/js/app.js
+++ web/js/app.js
@@ -14,3 +14,19 @@ function maximize() {
 function closeWindow() {
   remote.getCurrentWindow().close();
 }
+
+function addGame() {
+  var nameOfGame = $('#gameNameInput')[0].value
+
+  var $a = $("<a>", {
+    "class": "list-group-item list-group-item-action",
+    "href": "#"
+  })
+  $a.html(nameOfGame)
+  $('#game-list').append($a)
+
+  $('#new-game-modal').modal('hide')
+  $('#gameNameInput')[0].value = null
+
+  return false;
+}
